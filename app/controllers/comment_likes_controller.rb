f411e9ed74afddf0543ee140dfd983008a560604
飯野和輝 <iinokazuteru@MacBook-Pro-6.local>
2021-12-15 20:39:03 +0900

    comment_likeの非同期通信がうまく出来ない問題を解消

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -1,10 +1,12 @@
 class CommentLikesController < ApplicationController
     def create
 		@comment = Comment.find(params[:comment_id])
+		@post = Post.find(params[:post_id])
         comment_like = current_user.comment_likes.create(comment_id: params[:comment_id])
 	end
 	def destroy
 		@comment = Comment.find(params[:comment_id])
+		@post = Post.find(params[:post_id])
 		comment_like = CommentLike.find_by(comment_id: params[:comment_id], user_id: current_user.id)
 		comment_like.destroy
 	end
